89f6048c00d7b459700ae6c2cdb2bad0f01c6aa8
André Coutinho <andre9heber@gmail.com>
2026-03-23 20:58:02 -0300

    cadastro full

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -58,13 +58,15 @@ TEMPLATES = [
         'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
-            'context_processors': [
+            'context_processors': 
+            [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
-    },
+},
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
